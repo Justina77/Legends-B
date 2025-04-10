05b183d8932499389ca564e2c4afe4b8805829b5
Justina77 <petrushinajustina@gmail.com>
2025-04-10 14:32:59 +0300

    girl move

--- Legends B/Assets/Scripts/PlayerControl.cs
+++ Legends B/Assets/Scripts/PlayerControl.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private LayerMask;
-    // Start is called before the first frame update
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float moveSpeed = 7;
+
+    private CharacterController characterController;
+    private Vector3 targetPosition;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        targetPosition = transform.position;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        float distToTarget = Vector3.Distance(targetPosition, transform.position);
+        if(distToTarget > 1)
+        {
+            Vector3 direction = Vector3.Normalize(targetPosition- transform.position );
+            characterController.Move(direction* moveSpeed* Time.deltaTime);
+        }
+
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -21,6 +33,8 @@ public class PlayerControl : MonoBehaviour
             if(Physics.Raycast(ray, out hit, 500, layerMask))
             {
                 Debug.Log("hit: " + hit.collider.name);
+                targetPosition = hit.point;
+                transform.LookAt(targetPosition);
             }
         }
     }
